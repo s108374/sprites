64bcbcf502d98383309317e80029a97a1985bfa9
s108374 <92702181+s108374@users.noreply.github.com>
2021-10-18 12:38:59 +1000

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,94 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "sustainableSally":return img`
+. f f f . f f f f . f f f . 
+f f 5 f f 5 5 5 5 f f 5 f f 
+f 5 3 5 5 5 5 5 5 5 5 3 5 f 
+f f 5 5 5 5 5 5 5 5 5 5 f f 
+. f 5 5 5 5 5 5 5 5 5 5 f . 
+. f 5 5 5 5 d d 5 5 5 5 f . 
+. f f 5 5 d d d d 5 5 f f . 
+. f f f 9 f d d f 9 f f f . 
+. f f d f f d d f f d f f . 
+. . f f d d d d 2 d f f . . 
+. . 6 f 6 d 2 2 d 6 f 6 . . 
+. 6 6 f 6 6 6 6 6 6 f 6 6 . 
+. d 1 f 7 7 7 7 7 7 f 1 d . 
+. d d 7 7 7 7 7 7 7 7 d d . 
+. . 7 7 7 7 7 7 7 7 7 7 . . 
+. . . . f f . . f f . . . . 
+`;
+            case "image2":
+            case "rubbishBin":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+f f f . . . . . . . . . . f f f 
+f 6 f f f f f f f f f f f f 6 f 
+f 6 6 6 6 6 6 6 6 6 6 6 6 6 6 f 
+f f 6 7 7 7 7 7 7 7 7 7 7 6 f f 
+. f 6 7 7 7 7 5 5 7 7 7 7 6 f . 
+. f 6 7 7 7 5 7 7 5 7 7 7 6 f . 
+. f 6 7 7 7 7 7 7 7 7 7 7 6 f . 
+. f 6 7 7 5 7 7 7 7 5 7 7 6 f . 
+. f 6 7 5 7 7 7 7 7 7 5 7 6 f . 
+. f 6 7 7 5 7 5 5 7 5 7 7 6 f . 
+. f 6 7 7 7 7 7 7 7 7 7 7 6 f . 
+`;
+            case "image3":
+            case "chainsawCrowd":return img`
+......ffff..............
+....fff44fff............
+...fff4444fff...........
+..fffeeeeeefff..........
+..ffe444444eef..........
+..fe4ffffff4ef..........
+..ffffeeeeffff..........
+.ffef1fddf1feff.........
+.ffef2fddf2feff.........
+.feeddd22dddeef.bbbbbbbb
+fcfeed2dd2deeffbfffffffb
+fcffeeddddeecfddbbfbfbfb
+fcfdfccccccfceddebfbfbfb
+fdf.fccccccfccee.bbbbbbb
+fff.fddffddf............
+....ffffffff............
+`;
+            case "image4":
+            case "sustainableSally0":return img`
+. f f f . f f f f . f f f . 
+f f 5 f f 5 5 5 5 f f 5 f f 
+f 5 3 5 5 5 5 5 5 5 5 3 5 f 
+f f 5 5 5 5 5 5 5 5 5 5 f f 
+. f 5 5 5 5 5 5 5 5 5 5 f . 
+. f 5 5 5 5 d d 5 5 5 5 f . 
+. f f 5 5 d d d d 5 5 f f . 
+. f f f 9 f d d f 9 f f f . 
+. f f d f f d d f f d f f . 
+. . f f d d d d 2 d f f . . 
+. . 6 f 6 d 2 2 d 6 f 6 . . 
+. 6 6 f 6 6 6 6 6 6 f 6 6 . 
+. d 1 f 7 7 7 7 7 7 f 1 d . 
+. d d 7 7 7 7 7 7 7 7 d d . 
+. . 7 7 7 7 7 7 7 7 7 7 . . 
+. . . . f f . . f f . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,21 @@
+let mySprite = sprites.create(assets.image`sustainableSally`, SpriteKind.Player)
+let mySprite2 = sprites.create(assets.image`rubbishBin`, SpriteKind.Player)
+let mySprite3 = sprites.create(assets.image`chainsawCrowd`, SpriteKind.Player)
+let mySprite4 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . d d . . . . . . 
+    . . . . . d d f d d d . . . . . 
+    . . . e d f d d d d f d e . . . 
+    . . . e e d d d f d e e . . . . 
+    . . . . e e f d d e . . . . . . 
+    . . . . . e d f e e . . . . . . 
+    . . . f f f f f f . . . . . . . 
+    . . f f f f e e e e e . . . . . 
+    . f f e f e e 7 e e e e . . . . 
+    . f e e f e 7 7 7 e e e e . . . 
+    . . e e e e e 4 e e e e e . . . 
+    . . . e e e e e e e e e . . . . 
+    `, SpriteKind.Player)
